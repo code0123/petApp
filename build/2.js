a548c0f603f00f515845b70c9e7b6711c76720f9
doms123 <dominicksanchez30@gmail.com>
2018-01-11 12:17:25 +0800

    updates

--- build/2.js
+++ build/2.js
@@ -1,6 +1,6 @@
 webpackJsonp([2],{
 
-/***/ 536:
+/***/ 535:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -8,7 +8,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SearchvetPageModule", function() { return SearchvetPageModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(62);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__searchvet__ = __webpack_require__(547);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__searchvet__ = __webpack_require__(546);
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -38,7 +38,7 @@ var SearchvetPageModule = (function () {
 
 /***/ }),
 
-/***/ 547:
+/***/ 546:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -62,6 +62,7 @@ var SearchvetPage = (function () {
     function SearchvetPage(navCtrl, geolocation) {
         this.navCtrl = navCtrl;
         this.geolocation = geolocation;
+        this.pageLoaded = false;
     }
     SearchvetPage.prototype.ionViewDidEnter = function () {
         var _this = this;
@@ -69,7 +70,8 @@ var SearchvetPage = (function () {
             _this.getUserPosition();
         }, 400);
     };
-    SearchvetPage.prototype.getRestaurants = function (latLng) {
+    SearchvetPage.prototype.getVetClinic = function (latLng) {
+        var _this = this;
         var service = new google.maps.places.PlacesService(this.map);
         var request = {
             location: latLng,
@@ -79,7 +81,7 @@ var SearchvetPage = (function () {
         return new Promise(function (resolve, reject) {
             service.nearbySearch(request, function (results, status) {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    console.log('results', results);
+                    _this.pageLoaded = true;
                     resolve(results);
                 }
                 else {
@@ -88,22 +90,13 @@ var SearchvetPage = (function () {
             });
         });
     };
-    SearchvetPage.prototype.createMarker = function (place) {
-        var marker = new google.maps.Marker({
-            map: this.map,
-            animation: google.maps.Animation.DROP,
-            position: place.geometry.location
-        });
-    };
     SearchvetPage.prototype.getUserPosition = function () {
         var _this = this;
         this.options = {
             enableHighAccuracy: true
         };
-        console.log('options', this.options);
         this.geolocation.getCurrentPosition(this.options).then(function (pos) {
             _this.currentPos = pos;
-            console.log(pos);
             _this.addMap(pos.coords.latitude, pos.coords.longitude);
         }, function (err) {
             console.log("error : " + err.message);
@@ -114,17 +107,34 @@ var SearchvetPage = (function () {
         var latLng = new google.maps.LatLng(lat, long);
         var mapOptions = {
             center: latLng,
-            zoom: 20,
+            zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-        this.getRestaurants(latLng).then(function (results) {
+        this.getVetClinic(latLng).then(function (results) {
             _this.places = results;
             for (var i = 0; i < results.length; i++) {
                 _this.createMarker(results[i]);
             }
         }, function (status) { return console.log(status); });
         this.addMarker();
+    };
+    SearchvetPage.prototype.createMarker = function (place) {
+        var _this = this;
+        var marker = new google.maps.Marker({
+            map: this.map,
+            animation: google.maps.Animation.DROP,
+            position: place.geometry.location
+        });
+        var placeName = place.name;
+        var vicinity = place.vicinity;
+        var content = placeName + " (" + vicinity + ")";
+        var infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(_this.map, marker);
+        });
     };
     SearchvetPage.prototype.addMarker = function () {
         var _this = this;
@@ -143,15 +153,16 @@ var SearchvetPage = (function () {
     };
     __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_8" /* ViewChild */])('map'),
-        __metadata("design:type", __WEBPACK_IMPORTED_MODULE_0__angular_core__["t" /* ElementRef */])
+        __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_0__angular_core__["t" /* ElementRef */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_0__angular_core__["t" /* ElementRef */]) === "function" && _a || Object)
     ], SearchvetPage.prototype, "mapElement", void 0);
     SearchvetPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-searchvet',template:/*ion-inline-start:"C:\Users\ph2150108\Dropbox\petApp\src\pages\searchvet\searchvet.html"*/'<ion-header>\n    <ion-navbar>\n      <ion-title>Nearest Vet Clinics</ion-title>\n    </ion-navbar>\n</ion-header>\n  \n  \n<ion-content>\n  <div #map id="map"></div>\n  <div style="width : 100% ;height: 60%">\n    <ion-list>\n      <ion-item *ngFor="let place of places">\n        <p>{{place.name}}</p>\n      </ion-item>\n    </ion-list>\n  </div>\n</ion-content>\n  '/*ion-inline-end:"C:\Users\ph2150108\Dropbox\petApp\src\pages\searchvet\searchvet.html"*/,
+            selector: 'page-searchvet',template:/*ion-inline-start:"C:\Users\ph2150108\Dropbox\petApp\src\pages\searchvet\searchvet.html"*/'<ion-header>\n    <ion-navbar>\n      <ion-title>Nearest Vet Clinics</ion-title>\n    </ion-navbar>\n</ion-header>\n  \n  \n<ion-content>\n  <ion-spinner name="crescent" class="pageLoader" *ngIf="pageLoaded == false"></ion-spinner>\n  <div #map id="map"></div>\n  <div *ngIf="pageLoaded" style="width : 100% ;height: 60%">\n    <ion-list>\n      <ion-item *ngFor="let place of places">\n        <p>{{place.name}}</p>\n      </ion-item>\n    </ion-list>\n  </div>\n</ion-content>\n  '/*ion-inline-end:"C:\Users\ph2150108\Dropbox\petApp\src\pages\searchvet\searchvet.html"*/,
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */], __WEBPACK_IMPORTED_MODULE_2__ionic_native_geolocation__["a" /* Geolocation */]])
+        __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavController */]) === "function" && _b || Object, typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_2__ionic_native_geolocation__["a" /* Geolocation */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__ionic_native_geolocation__["a" /* Geolocation */]) === "function" && _c || Object])
     ], SearchvetPage);
     return SearchvetPage;
+    var _a, _b, _c;
 }());
 
 //# sourceMappingURL=searchvet.js.map
--- build/main.js
+++ build/main.js
@@ -131,35 +131,35 @@ var map = {
 		9
 	],
 	"../pages/lostpets/lostpets.module": [
-		534,
+		529,
 		8
 	],
 	"../pages/other/other.module": [
-		529,
+		530,
 		7
 	],
 	"../pages/pet/pet.module": [
-		530,
+		531,
 		6
 	],
 	"../pages/profile/profile.module": [
-		531,
+		532,
 		5
 	],
 	"../pages/register/register.module": [
-		532,
+		533,
 		4
 	],
 	"../pages/registerpets/registerpets.module": [
-		533,
+		534,
 		3
 	],
 	"../pages/searchvet/searchvet.module": [
-		536,
+		535,
 		2
 	],
 	"../pages/tabs/tabs.module": [
-		535,
+		536,
 		1
 	],
 	"../pages/trainpets/trainpets.module": [
@@ -544,14 +544,14 @@ var AppModule = (function () {
                         { loadChildren: '../pages/editprofile/editprofile.module#EditprofilePageModule', name: 'EditprofilePage', segment: 'editprofile', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/home/home.module#HomePageModule', name: 'HomePage', segment: 'home', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/login/login.module#LoginPageModule', name: 'LoginPage', segment: 'login', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/lostpets/lostpets.module#LostpetsPageModule', name: 'LostpetsPage', segment: 'lostpets', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/other/other.module#OtherPageModule', name: 'OtherPage', segment: 'other', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/pet/pet.module#PetPageModule', name: 'PetPage', segment: 'pet', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/profile/profile.module#ProfilePageModule', name: 'ProfilePage', segment: 'profile', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/register/register.module#RegisterPageModule', name: 'RegisterPage', segment: 'register', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/registerpets/registerpets.module#RegisterpetsPageModule', name: 'RegisterpetsPage', segment: 'registerpets', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/lostpets/lostpets.module#LostpetsPageModule', name: 'LostpetsPage', segment: 'lostpets', priority: 'low', defaultHistory: [] },
-                        { loadChildren: '../pages/tabs/tabs.module#TabsPageModule', name: 'TabsPage', segment: 'tabs', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/searchvet/searchvet.module#SearchvetPageModule', name: 'SearchvetPage', segment: 'searchvet', priority: 'low', defaultHistory: [] },
+                        { loadChildren: '../pages/tabs/tabs.module#TabsPageModule', name: 'TabsPage', segment: 'tabs', priority: 'low', defaultHistory: [] },
                         { loadChildren: '../pages/trainpets/trainpets.module#TrainpetsPageModule', name: 'TrainpetsPage', segment: 'trainpets', priority: 'low', defaultHistory: [] }
                     ]
                 }),
